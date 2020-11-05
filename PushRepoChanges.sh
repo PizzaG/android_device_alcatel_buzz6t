4ae396e54bedf99ec7dc682d0b615f3fb8b8c314
PizzaG <pizzag98@gmail.com>
2020-11-05 17:27:18 -0600

    Updated Repo Push Script

--- PushRepoChanges.sh
+++ PushRepoChanges.sh
@@ -6,26 +6,27 @@
 git status
 echo ""
 
-
-
-
 git add .
-
-#git commit -m "Updates"
 echo ""
+
 echo " Press Enter To Continue"
+echo ""
 read git_commit_msg
+echo ""
 
 echo "Enter Your git commit Message..."
 echo $git_commit_msg
+echo ""
 
 export $git_commit_msg
+echo ""
 
 git commit $git_commit_msg
+echo ""
 
 git push
-
 echo ""
+
 echo " Repo Changes Should Be Pushed"
 echo ""
 echo "Press Enter To Exit"
